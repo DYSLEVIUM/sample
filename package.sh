a407910611c24ebc665dd06a4940b78651d64011
Anand <akmali@cgbu-phx-215.snphxprshared1.gbucdsint02phx.oraclevcn.com>
2023-06-19 11:53:55 +0000

    test npm publish

--- package.sh
+++ package.sh
@@ -1,17 +1,13 @@
+#!/bin/bash
+
 source_dir="dist"
 
 # Create a temporary directory to store the tar file
 temp_dir=$(mktemp -d)
 
-find "$source_dir" -type f -name "*.map" -exec tar -rf "$temp_dir/archive.tar" {} +
+# remove all the other files except file with *.map from dist folder
+find "$source_dir" -type f ! -name "*.map" -exec rm -rf {} \;
 
 # Tar the entire directory structure
-tar -cf "$temp_dir/ecprt-livekit-client-sdk-js-${VERSION}.tar.gz" "$source_dir" && echo "Added directory: $source_dir"
+tar -czf ecprt-livekit-client-sdk-js-${VERSION}.tar.gz "$source_dir"
 
-# Move the tar file to the desired location
-mv "$temp_dir/ecprt-livekit-client-sdk-js-${VERSION}.tar.gz" "ecprt-livekit-client-sdk-js-${VERSION}.tar.gz"
-
-# Clean up the temporary directory
-rm -r "$temp_dir"
-
-echo "Tar file created successfully."
