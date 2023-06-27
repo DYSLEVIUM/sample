df5393140eb1aca09605dee712ea3f6590bb99e8
Anand <akmali@cgbu-phx-215.snphxprshared1.gbucdsint02phx.oraclevcn.com>
2023-06-27 13:14:28 +0000

    rename pack

--- rename_pack.sh
+++ rename_pack.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-build_version=${?BLD_BRANCH_SUFFIX}.${BLD_NUMBER}
+branch_name=BLD_BRANCH_SUFFIX
+bld_num=BLD_NUMBER 
 
-mv ecprt-client-sdk-0.0.1.tgz ecprt-client-sdk-0.0.1-${build_version}.tgz
+mv ecprt-client-sdk-0.0.1.tgz ecprt-client-sdk-0.0.1-${branch_name}.${bld_num}.tgz
